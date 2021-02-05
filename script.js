69561d9cd18bd542cb1a95d6b953e70be7a756bd
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-02-05 17:14:44 +0530

    stopped design

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ const bass = document.getElementById('bass')
 const mid = document.getElementById('mid')
 const treble = document.getElementById('treble')
 const visualizer = document.getElementById('visualizer')
-const start = document.getElementById("start");
-const stop = document.getElementById("stop");
-const video = document.querySelector("video");
-const download = document.querySelector("a");
-let recorder, stream;
+// const start = document.getElementById("start");
+// const stop = document.getElementById("stop");
+// const video = document.querySelector("video");
+// const download = document.querySelector("a");
+// let recorder, stream;
 
 
 const context = new AudioContext()
@@ -113,36 +113,36 @@ function resize() {
   visualizer.height = visualizer.clientHeight * window.devicePixelRatio
 }
 
-//media streaming
+// //media streaming
 
-async function startRecording() {
-    stream = await navigator.mediaDevices.getDisplayMedia({
-      video: { mediaSource: "screen" }
-    });
-    recorder = new MediaRecorder(stream);
+// async function startRecording() {
+//     stream = await navigator.mediaDevices.getDisplayMedia({
+//       video: { mediaSource: "screen" }
+//     });
+//     recorder = new MediaRecorder(stream);
   
-    const chunks = [];
-    recorder.ondataavailable = e => chunks.push(e.data);
-    recorder.onstop = e => {
-      const completeBlob = new Blob(chunks, { type: chunks[0].type });
-      video.src = URL.createObjectURL(completeBlob);
-    };
+//     const chunks = [];
+//     recorder.ondataavailable = e => chunks.push(e.data);
+//     recorder.onstop = e => {
+//       const completeBlob = new Blob(chunks, { type: chunks[0].type });
+//       video.src = URL.createObjectURL(completeBlob);
+//     };
   
-    recorder.start();
-  }
+//     recorder.start();
+//   }
   
-  start.addEventListener("click", () => {
-    start.setAttribute("disabled", true);
-    stop.removeAttribute("disabled");
+//   start.addEventListener("click", () => {
+//     start.setAttribute("disabled", true);
+//     stop.removeAttribute("disabled");
   
-    startRecording();
-  });
+//     startRecording();
+//   });
   
-  stop.addEventListener("click", () => {
-    stop.setAttribute("disabled", true);
-    start.removeAttribute("disabled");
+//   stop.addEventListener("click", () => {
+//     stop.setAttribute("disabled", true);
+//     start.removeAttribute("disabled");
   
-    recorder.stop();
-    stream.getVideoTracks()[0].stop();
-  });
+//     recorder.stop();
+//     stream.getVideoTracks()[0].stop();
+//   });
   
